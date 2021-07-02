d93a1282e633fb9fdb8b47551cb1c60325c77f13
psuobfuscator <79279669+psuobfuscator@users.noreply.github.com>
2021-07-02 15:15:57 +0300

    Add files via upload

---
+++ 2.js
@@ -0,0 +1,15 @@
+// Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyDBIQU4TclfLG5Lp6p7w-tfZEJGQVUZr-w",
+    authDomain: "bypasser-b2dc0.firebaseapp.com",
+    databaseURL: "https://bypasser-b2dc0-default-rtdb.firebaseio.com",
+    projectId: "bypasser-b2dc0",
+    storageBucket: "bypasser-b2dc0.appspot.com",
+    messagingSenderId: "787076908751",
+    appId: "1:787076908751:web:c2b8d7efa6f10e98d699ba",
+    measurementId: "G-6EMFZLWE27"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
