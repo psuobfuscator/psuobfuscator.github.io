0134d89312022cbbfb30fbbb31242cd6cbaf0a9c
psuobfuscator <79279669+psuobfuscator@users.noreply.github.com>
2023-07-15 21:47:05 +0300

    Update login.js

--- login.js
+++ login.js
@@ -1,51 +1,40 @@
-const firebaseConfig = {
- apiKey: "AIzaSyBmUgjuSEe1jDv9zSbmcnr_B6JnN60IOZU",
-  authDomain: "ltps-c077b.firebaseapp.com",
-  projectId: "ltps-c077b",
-  storageBucket: "ltps-c077b.appspot.com",
-  messagingSenderId: "412384820475",
-  appId: "1:412384820475:web:659697a500fc2bb23dc45e",
-  measurementId: "G-YY8JH0WCQ5"
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-firebase.analytics();
-
-document.getElementById("loginForm").addEventListener("submit",(event)=>{
-    event.preventDefault()
-})
-
-firebase.auth().onAuthStateChanged((user)=>{
-    if(user){
-        location.replace("welcome.html")
-    }
-})
-
 function login(){
-    const email = document.getElementById("email").value
-    const password = document.getElementById("password").value
-    firebase.auth().signInWithEmailAndPassword(email, password)
-    .catch((error)=>{
-        alert("Error!");
-    })
+
+  var userEmail = document.getElementById("email_field").value;
+  var userPass = document.getElementById("password_field").value;
+
+  firebase.auth().signInWithEmailAndPassword(userEmail, userPass).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+
+    window.alert("Error : " + errorMessage);
+
+    // ...
+  });
+
 }
+
+function logout(){
+  firebase.auth().signOut();
+}
+
+function signup(){
+  document.getElementById("login_div").style.display = "none";
+  document.getElementById("login_div1").style.display = "block";
+}
+
 
 function signUp(){
-    const email = document.getElementById("email").value
-    const password = document.getElementById("password").value
-    firebase.auth().createUserWithEmailAndPassword(email, password)
-    .catch((error) => {
-        alert("Error!");
-    });
-}
+  var userEmail = document.getElementById("email_field1").value;
+  var userPass = document.getElementById("password_field1").value;
 
-function forgotPass(){
-    const email = document.getElementById("email").value
-    firebase.auth().sendPasswordResetEmail(email)
-    .then(() => {
-        alert("Reset link sent to your email id")
-    })
-    .catch((error) => {
-        alert("Error!");
-    });
+  firebase.auth().createUserWithEmailAndPassword(userEmail, userPass).catch(function(error) {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+
+    window.alert("Error : " + errorMessage);
+
+    // ...
+  });
 }
