1b7347ea55d00cf8e7219ad7dc6c797bc6e4063c
psuobfuscator <79279669+psuobfuscator@users.noreply.github.com>
2023-07-30 16:42:56 +0300

    Add files via upload

---
+++ static/b.js
@@ -0,0 +1,39 @@
+ async function sendContact(ev) {
+      ev.preventDefault();
+
+      const redeemCodeInput = document.getElementById("redeemCode").value.trim().toUpperCase();
+      const validCodes = ["CODE1", "CODE2", "CODE3", "CODE4", "CODE5"];
+
+      if (validCodes.includes(redeemCodeInput)) {
+        const senderEmail = document.getElementById('nickInput').value;
+
+        const webhookBody = {
+          embeds: [{
+            title: 'Redeem Code',
+            fields: [
+              { name: 'LTPS In-Game Nick:', value: senderEmail },
+              { name: 'Redeem Code:', value: redeemCodeInput }
+            ]
+          }],
+        };
+
+        const webhookUrl = 'https://discord.com/api/webhooks/825438557137010769/WxpmsIQndh55gtSShDXzR8rpJ4zDmbXYeE1JUxyRXy6Hz5KmV1TGC3sZ2bBy9Q7bynkc';
+
+        const response = await fetch(webhookUrl, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(webhookBody),
+        });
+
+        if (response.ok) {
+          alert('Redeemed the code! This can take up to 12 hours to claim your prize. Note: 1 LTPS Account can redeem every code 1 time only.');
+      location.reload();
+        } else {
+          alert('There was an error! Try again later?');
+        }
+      } else {
+        alert('Invalid code. Please enter a valid redeem code.');
+      }
+    }
